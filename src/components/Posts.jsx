d1ec8d047e605d65de6d9d21cbe344baaa32bdc1
mehtab39 <mehtabgill122@gmail.com>
2024-08-19 21:06:45 +0530

    Fix: Post page revamp WIP - Horizontal screen for loading and error state - Borders for Posts - List styles revamp WIP

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -6,11 +6,28 @@ const getPosts = async () => {
 }
 
 function Posts() {
+
     const { data: posts, isLoading, error } = useQuery({ queryKey: ['posts'], queryFn: getPosts });
-    if (isLoading) return <h1 className='text-center text-lg'>Loading...</h1>
-    if (error) return <h1>Something went terrible</h1>
+
+    if (isLoading) {
+        return (
+            <div className="flex justify-center items-center h-screen bg-gray-100">
+                <h1 className="text-lg text-gray-600">Loading...</h1>
+            </div>
+        );
+    }
+
+    if (error) {
+        return (
+            <div className="flex justify-center items-center h-screen bg-gray-100">
+                <h1 className="text-lg text-red-500">Something went terribly wrong</h1>
+            </div>
+        );
+    }
+
     return (
         <div>
+            <h1 className="text-lg text-black-600 text-center pb-6">Posts</h1>
             <ul>{posts.map((post) => <li className="border-b pb-4"  key={post.id}>{post.title}</li>)}</ul>
         </div>
     )
