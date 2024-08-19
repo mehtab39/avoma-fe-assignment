fdf1a0121f7de0bc08701cb299b062781daa2084
mehtab39 <mehtabgill122@gmail.com>
2024-08-19 21:24:55 +0530

    Fix: styling for Posts List page complete - Added hover and click handling for Post title - Added tailwind utility classes for enhancing UI

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,4 +1,5 @@
 import { useQuery, } from '@tanstack/react-query';
+import { Link } from 'react-router-dom';
 
 const getPosts = async () => {
     const response = await fetch('https://jsonplaceholder.typicode.com/posts');
@@ -26,9 +27,18 @@ function Posts() {
     }
 
     return (
-        <div>
-            <h1 className="text-lg text-black-600 text-center pb-6">Posts</h1>
-            <ul>{posts.map((post) => <li className="border-b pb-4"  key={post.id}>{post.title}</li>)}</ul>
+        <div className="max-w-4xl mx-auto mt-10">
+            <h1 className="text-3xl font-bold text-center text-gray-800 mb-6">Posts</h1>
+            <ul className="space-y-4">
+                {posts.map((post) => (
+                    <li key={post.id} className="border-b pb-4">
+                        <Link to={`/posts/${post.id}`} className="text-xl font-bold text-blue-500 hover:underline">
+                            {post.title}
+                        </Link>
+                        <p className="mt-2 text-gray-700">{post.body}</p>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
