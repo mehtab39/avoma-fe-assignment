43d422d30c631d70abb18927d90e96163c23d09c
mehtab39 <mehtabgill122@gmail.com>
2024-08-19 20:12:55 +0530

    Add: QueryClient initialising in the project  - initialise query client and wrap App with QueryClientProvider  - created getPosts method to fetch posts from API  - initial markup to showcase posts from the API

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import './App.css'
+import { useQuery,} from '@tanstack/react-query';
+
+const getPosts = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  return response.json();
+}
 
 function App() {
-  return <h1>Avoma Frontend project Init</h1>
+  const { data: posts, isLoading, error } = useQuery({ queryKey: ['posts'], queryFn: getPosts });
+  if(isLoading) return <h1>Loading...</h1>
+  if(error) return <h1>Something went terrible</h1>
+  return (
+    <div>
+      <ul>{posts.map((post) => <li key={post.id}>{post.title}</li>)}</ul>
+    </div>
+  )
 }
 
 export default App
