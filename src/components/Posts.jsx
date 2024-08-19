651c87657ea1a3f575ce7ca5eb04e3820f8aa824
mehtab39 <mehtabgill122@gmail.com>
2024-08-19 20:43:58 +0530

    Refactor CSS and tailwind utilities - Removed unused CSS and App.css file - Configured tailwind config file - Basic styling for Posts page

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -7,11 +7,11 @@ const getPosts = async () => {
 
 function Posts() {
     const { data: posts, isLoading, error } = useQuery({ queryKey: ['posts'], queryFn: getPosts });
-    if (isLoading) return <h1>Loading...</h1>
+    if (isLoading) return <h1 className='text-center text-lg'>Loading...</h1>
     if (error) return <h1>Something went terrible</h1>
     return (
         <div>
-            <ul>{posts.map((post) => <li key={post.id}>{post.title}</li>)}</ul>
+            <ul>{posts.map((post) => <li className="border-b pb-4"  key={post.id}>{post.title}</li>)}</ul>
         </div>
     )
 }
